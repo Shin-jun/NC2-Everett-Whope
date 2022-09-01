c6031f428b59024f0712bdb6b847aefe8b5e0af1
Shin-jun <inside1451@naver.com>
2022-09-01 15:36:33 +0900

    Initial commit

---
+++ WhoPeople/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  WhoPeople
+//
+//  Created by Shin yongjun on 2022/08/31.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+
+}
+
---
+++ WhoPeople/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  WhoPeople
+//
+//  Created by Shin yongjun on 2022/08/31.
+//
+
+import UIKit
+import AVFoundation
+import Vision
+
+class ViewController: UIViewController {
+    
+}
+
